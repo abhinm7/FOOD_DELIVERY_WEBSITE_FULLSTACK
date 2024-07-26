90ee6bbeddb05c2ae31306d78e844e6b7928e213
ABHIN M <abhin18m@gmail.com>
2024-07-26 15:45:50 +0530

    fetched data to frontend

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -5,12 +5,12 @@ import { StoreContext } from '../../context/StoreContext';
 
 const FoodItem = ({id,name,price,description,image}) => {
 
-    const {cartItems,addToCart,removeFromCart} = useContext(StoreContext);
+    const {cartItems,addToCart,removeFromCart,url} = useContext(StoreContext);
 
   return (
     <div className='food-item'>
         <div className="food-item-image-container">
-            <img src={image} className='food-item-image' alt="" />
+            <img src={url+"/images/"+image} className='food-item-image' alt="" />
             {
                 !cartItems[id]
                 ?
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,65 +1,74 @@
 import { createContext, useEffect, useState } from "react";
-import { food_list } from "../assets/assets";
+import axios from 'axios'
 
 export const StoreContext = createContext(null)
 
-const StoreContextProvider = (props)=>{
+const StoreContextProvider = (props) => {
 
-    const [cartItems,setCartItems] = useState({});
-    const url = "http://localhost:5000" 
-    const [token,setToken] = useState("")
+    const [cartItems, setCartItems] = useState({});
+    const url = "http://localhost:5000"
+    const [token, setToken] = useState("")
+    const [food_list, setFoodlist] = useState([])
 
-    const addToCart = (itemId) =>{
-        if(!cartItems[itemId]){
-            setCartItems((prev)=>({...prev,[itemId]:1}))
+    const addToCart = (itemId) => {
+        if (!cartItems[itemId]) {
+            setCartItems((prev) => ({ ...prev, [itemId]: 1 }))
         }
-        else{
-            setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+        else {
+            setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
         }
     }
 
-    const removeFromCart = (itemId)=>{
-        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+    const removeFromCart = (itemId) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
     }
 
-    const getTotalAmount=()=>{
+    const getTotalAmount = () => {
         let totalAmount = 0;
-        for(const item in cartItems){
-            if(cartItems[item]>0){
-                let itemInfo = food_list.find((product)=>product._id === item);
-                totalAmount += itemInfo.price*cartItems[item];
+        for (const item in cartItems) {
+            if (cartItems[item] > 0) {
+                let itemInfo = food_list.find((product) => product._id === item);
+                totalAmount += itemInfo.price * cartItems[item];
             }
         }
         return totalAmount;
 
     }
-
-    useEffect(()=>{
-        if(localStorage.getItem("token")){
-       setToken(localStorage.getItem("token"))
-        }
-    },[])
-
-    useEffect(()=>{
-       console.log(cartItems);
-    },[cartItems])
-
-    const contextValue ={
-    food_list,
-    cartItems,
-    setCartItems,
-    addToCart,
-    removeFromCart,
-    getTotalAmount,
-    url,
-    token,
-    setToken
+    const fetchFoodList = async () => {
+        const response = await axios.get(url + "/api/food/list")
+        setFoodlist(response.data.data)
     }
-            return(
-                <StoreContext.Provider value={contextValue}>
-                {props.children}
-                </StoreContext.Provider>
-            )
+
+    useEffect(() => {
+        async function loadData() {
+            await fetchFoodList();
+            if (localStorage.getItem("token")) {
+                setToken(localStorage.getItem("token"))
+            }
+        }
+       loadData();
+    }, [])
+
+    useEffect(() => {
+        console.log(cartItems);
+    }, [cartItems])
+
+    const contextValue = {
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart,
+        getTotalAmount,
+        url,
+        token,
+        setToken
+    }
+    return (
+        <StoreContext.Provider value={contextValue}>
+            {props.children}
+        </StoreContext.Provider>
+    )
 }
 
 export default StoreContextProvider
