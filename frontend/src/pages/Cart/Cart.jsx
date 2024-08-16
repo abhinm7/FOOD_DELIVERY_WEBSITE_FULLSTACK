8a8df99ed844b445ea057778ee4db6007194db06
ABHIN M <abhin18m@gmail.com>
2024-08-16 18:04:50 +0530

    test commmit

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,12 +1,11 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import './Cart.css'
 import { StoreContext } from '../../context/StoreContext';
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const cart = () => {
-  const {cartItems,food_list,removeFromCart,getTotalAmount,url} = useContext(StoreContext);
+  const { cartItems, food_list, removeFromCart, getTotalAmount, url } = useContext(StoreContext);
 
-   const
 
   const navigate = useNavigate();
   return (
@@ -19,59 +18,60 @@ const cart = () => {
           <p>Quantity</p>
           <p>Total</p>
           <p>Remove</p>
-          </div>
-          <br />
-          <hr />
-          {
-          food_list.map((item,index)=>{
+        </div>
+        <br />
+        <hr />
+        {
+          food_list.map((item, index) => {
             let quantity = cartItems[item._id];
-            if(quantity>0){
-              return(
+            if (quantity > 0) {
+              return (
                 <div>
-                <div className="cart-items-title cart-items-item">
-                <img src={url+'/images/'+item.image} alt="" />
-                <p>{item.name}</p>
-                <p>{item.price}</p>
-                <p>{quantity}</p>
-                <p>{item.price*quantity}</p>
-                <p onClick={()=>{removeFromCart(item._id)}} className='cross'>X</p>
-              </div>
-              <hr />
-              </div>
+                  <div className="cart-items-title cart-items-item">
+                    <img src={url + '/images/' + item.image} alt="" />
+                    <p>{item.name}</p>
+                    <p>{item.price}</p>
+                    <p>{quantity}</p>
+                    <p>{item.price * quantity}</p>
+                    <p onClick={() => { removeFromCart(item._id) }} className='cross'>X</p>
+                  </div>
+                  <hr />
+                </div>
               )
             }
           })}
-        </div>
-        <div className="cart-bottom">
-          <div className="cart-total">
-            <h2>Cart Totals</h2>
-            <div>
-              <div className="cart-total-details">
-                <p>Subtotal</p>
-                <p>₹{getTotalAmount()}</p>
-              </div>
-              <hr />
-              <div className="cart-total-details">
-                <p>Delivey fee</p>
-                <p>₹{getTotalAmount()===0?0:20}</p>
-              </div>
-              <hr />
-              <div className="cart-total-details bold">
-                <p>Total</p>
-                <p>₹{getTotalAmount()===0?0:getTotalAmount()+20}</p>
-                </div>    
-              </div>
-                <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>        
-          </div>
-          <div className="cart-promocode">
-            <p>If you have a promocode , enter it here</p>
-            <div className="promocode-input">
-              <input type="text" placeholder='promo code' />
-              <button>Submit</button>
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>₹{getTotalAmount()}</p>
             </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivey fee</p>
+              <p>₹{getTotalAmount() === 0 ? 0 : 20}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details bold">
+              <p>Total</p>
+              <p>₹{getTotalAmount() === 0 ? 0 : getTotalAmount() + 20}</p>
+            </div>
+            
+          </div>
+          <button onClick={() => navigate('/order')}>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cart-promocode">
+          <p>If you have a promocode , enter it here</p>
+          <div className="promocode-input">
+            <input type="text" placeholder='enter promocode' />
+            <button>Submit</button>
           </div>
         </div>
       </div>
+    </div>
   )
 }
 
