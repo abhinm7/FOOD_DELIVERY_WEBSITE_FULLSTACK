d8c850de973fe8a2a06ffb55b4139ef7cfc76ed4
ABHIN M <abhin18m@gmail.com>
2024-08-16 19:31:42 +0530

    added copyright logo

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -36,7 +36,7 @@ const footer = () => {
       </div>
       <hr />
       <p className="footer-copyright">
-        copyright 2024 C abhin.m All rights reserved.
+        copyright 2024 © abhin.m All rights reserved.
       </p>
       </div>
   )
